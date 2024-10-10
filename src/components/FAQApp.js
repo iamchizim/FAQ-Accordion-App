8d02051f3e5ff4b4a6953a16a5ac2e313889cf92
Chukwuebuka Chidi-Ezirim <chizim2003@gmail.com>
2024-10-10 11:57:52 +0100

    created a FAQApp component

---
+++ src/components/FAQApp.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useState } from "react";
+
+const FAQApp = () => {
+  const [currentlyClicked, setCurrentlyClicked] = useState(null);
+
+  const handleCurrentlyClicked = (index) => {
+    if (currentlyClicked === index) {
+      setCurrentlyClicked(null);
+    } else {
+      setCurrentlyClicked(index);
+    }
+  };
+
+  const faqs = [
+    {
+      index: 1,
+      question: "What is your return policy?",
+      answer: "You can return items within 30 days.",
+    },
+    {
+      index: 2,
+      question: "How do I track my order?",
+      answer: "You can track your order through the link sent to your email.",
+    },
+    {
+      index: 3,
+      question: "Can I purchase items internationally?",
+      answer: "Yes, we ship internationally.",
+    },
+  ];
+
+  return (
+    <section>
+      {faqs.map((faq, index) => (
+        <div key={index}>
+          <h3 onClick={() => handleCurrentlyClicked(index)}>{faq.question}</h3>
+          {currentlyClicked === index && <p>{faq.answer}</p>}
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default FAQApp;
